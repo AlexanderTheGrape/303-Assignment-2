d3a99f6aeb650bc13c1c311125d01bdefc6c618c
Parie Malhotra <pmal123@UoA.auckland.ac.nz>
2019-09-21 16:58:22 +1200

    Set up Eclipse stuff

---
+++ Lab___export/Prebuilt_Nios/software/assignment2_pacemaker/obj/default/hello_world.d
@@ -0,0 +1,13 @@
+obj/default/hello_world.o: hello_world.c \
+ ../assignment2_pacemaker_bsp//HAL/inc/sys/alt_alarm.h \
+ ../assignment2_pacemaker_bsp//HAL/inc/sys/alt_llist.h \
+ ../assignment2_pacemaker_bsp//HAL/inc/alt_types.h \
+ ../assignment2_pacemaker_bsp//HAL/inc/priv/alt_alarm.h
+
+../assignment2_pacemaker_bsp//HAL/inc/sys/alt_alarm.h:
+
+../assignment2_pacemaker_bsp//HAL/inc/sys/alt_llist.h:
+
+../assignment2_pacemaker_bsp//HAL/inc/alt_types.h:
+
+../assignment2_pacemaker_bsp//HAL/inc/priv/alt_alarm.h:
