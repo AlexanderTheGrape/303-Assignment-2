f28317081308ffb0dd803890f285de463ab7f5eb
Alexander Monk <amon838@UoA.auckland.ac.nz>
2019-09-24 13:54:13 +1200

    mid tuesday

---
+++ Lab___export/Prebuilt_Nios/software/fresh_beets/obj/default/hello_world.d
@@ -0,0 +1,34 @@
+obj/default/hello_world.o: hello_world.c \
+ ../fresh_beets_bsp//HAL/inc/sys/alt_alarm.h \
+ ../fresh_beets_bsp//HAL/inc/sys/alt_llist.h \
+ ../fresh_beets_bsp//HAL/inc/alt_types.h \
+ ../fresh_beets_bsp//HAL/inc/priv/alt_alarm.h \
+ ../fresh_beets_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../fresh_beets_bsp//HAL/inc/io.h ../fresh_beets_bsp//HAL/inc/alt_types.h \
+ ../fresh_beets_bsp/system.h ../fresh_beets_bsp/linker.h \
+ ../fresh_beets_bsp//HAL/inc/sys/alt_irq.h \
+ ../fresh_beets_bsp//HAL/inc/nios2.h pacemaker.h
+
+../fresh_beets_bsp//HAL/inc/sys/alt_alarm.h:
+
+../fresh_beets_bsp//HAL/inc/sys/alt_llist.h:
+
+../fresh_beets_bsp//HAL/inc/alt_types.h:
+
+../fresh_beets_bsp//HAL/inc/priv/alt_alarm.h:
+
+../fresh_beets_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../fresh_beets_bsp//HAL/inc/io.h:
+
+../fresh_beets_bsp//HAL/inc/alt_types.h:
+
+../fresh_beets_bsp/system.h:
+
+../fresh_beets_bsp/linker.h:
+
+../fresh_beets_bsp//HAL/inc/sys/alt_irq.h:
+
+../fresh_beets_bsp//HAL/inc/nios2.h:
+
+pacemaker.h:
